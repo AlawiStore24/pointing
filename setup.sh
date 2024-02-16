c003b092f197976566ff1ed02bce872a628f37fd
AlawiStore24 <159411067+AlawiStore24@users.noreply.github.com>
2024-02-16 18:33:14 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#wget menu.sh
+clear
+wget https://raw.githubusercontent.com/AlawiStore24/pointing/main/menu.sh
+mv menu.sh /usr/local/sbin/menu
+chmod +x /usr/local/sbin/menu
+clear
+#wget pointing.sh
+wget https://raw.githubusercontent.com/AlawiStore24/pointing/main/pointing.sh
+mv pointing.sh /usr/local/sbin/pointing
+chmod +x /usr/local/sbin/pointing
+clear
+echo -e "penginstalan menu pointing domain selesai. silahkan ketik [ menu ] untuk menggunakannya"
